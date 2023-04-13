27f70287225cb633c052ac43927ed56ef8781b08
Pharm-Nelson <nelsoneke3@gmail.com>
2023-04-13 10:22:28 +0000

    new tasks

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -29,12 +29,3 @@ class Student():
         if (type(attrs) == list and all(type(ele) == str for ele in attrs)):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return (self.__dict__)
-
-    def reload_from_json(self, json):
-        '''Replaces all attributes of the Student instance.
-
-        Args:
-           json (dict): The key/value pairs to replace attributes with.
-        '''
-        for k, v, in json.items():
-            setattr(self, k, v)
--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,90 +1,38 @@
 #!/usr/bin/python3
-"""
-0x0B. Python - Input/Output, task 16. Log parsing
-
-Parses a log of HTTP GET request results from stdin to tabulate the total
-counts of status codes appearing in each response, and the total file size
-across all requests.
-
-Example of expected log line input:
-128.230.61.246 - [2017-02-05 23:31:23.258076] \
-"GET /projects/260 HTTP/1.1" 301 292
-
-Fields:
-<IP Address> - [<date>] "<GET request>" <response status code> <file size>
-"""
+import sys
 
 
-def print_log_totals(total_file_size, code_counts):
-    """
-    Prints current totals of file size and status code counts.
+def print_status():
+    '''
+        Printing the status of the request
+    '''
+    counter = 0
+    size = 0
+    file_size = 0
+    status_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
+                    "403": 0, "404": 0, "405": 0, "500": 0}
 
-    Args:
-        total_file_size (int): cumulative total of bytes received through
-            GET requests in log
-        code_counts (OrderedDict): totals of status codes from repsonses
-    """
-    print("File size: {}".format(total_file_size))
-    for code in code_counts:
-        if code_counts[code] > 0:
-            print("{}: {}".format(code, code_counts[code]))
+    for l in sys.stdin:
+        line = l.split()
+        try:
+            size += int(line[-1])
+            code = line[-2]
+            status_codes[code] += 1
+        except:
+            continue
+        if counter == 9:
+            print("File size: {}".format(size))
+            for key, val in sorted(status_codes.items()):
+                if (val != 0):
+                    print("{}: {}".format(key, val))
+            counter = 0
+        counter += 1
+    if counter < 9:
+        print("File size: {}".format(size))
+        for key, val in sorted(status_codes.items()):
+            if (val != 0):
+                print("{}: {}".format(key, val))
 
 
-if __name__ == '__main__':
-    from sys import argv, stdin, stderr
-    from collections import OrderedDict
-    from datetime import datetime
-
-    line_no = 0
-    total_file_size = 0
-    code_counts = OrderedDict.fromkeys([200, 301, 400, 401, 403,
-                                        404, 405, 500], 0)
-
-    try:
-        for line in stdin:
-            line_no += 1
-
-            # both ip addresses/URLs allowed, but some lines may be raw text
-            a = line.split('-', 1)
-            if len(a) != 2:
-                # likely not a formatted line
-                continue
-
-            # checking timestamp
-            b = a[1].split(']')
-            timecode = b[0].lstrip(' [')
-            try:
-                datetime.strptime(timecode, '%Y-%m-%d %H:%M:%S.%f')
-            except:
-                stderr.write("{}: {}: invalid timecode\n".format(
-                    argv[0], line_no))
-                pass
-
-            # checking URL
-            c = b[1].split('"')
-            c = c[1:]
-            if c[0] != 'GET /projects/260 HTTP/1.1':
-                stderr.write("{}: {}: unexpected HTTP request\n".format(
-                    argv[0], line_no))
-
-            # prep for status code and file size
-            d = c[1].lstrip(' ')
-            d = d.rstrip('\n')
-            d = d.split(' ')
-
-            # checking status code (invalid codes skipped without error)
-            if d[0].isdecimal():
-                code = int(d[0])
-                code_counts[code] += 1
-
-            # checking file size (invalid sizes skipped without error)
-            if d[1].isdecimal():
-                total_file_size += int(d[1])
-
-            if line_no % 10 == 0:
-                print_log_totals(total_file_size, code_counts)
-        print_log_totals(total_file_size, code_counts)
-
-    except (KeyboardInterrupt):
-        print_log_totals(total_file_size, code_counts)
-        raise
+if __name__ == "__main__":
+    print_status()
