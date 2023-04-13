8ea91ecb45c85f878d177485e113de0ee1916d4b
Pharm-Nelson <nelsoneke3@gmail.com>
2023-04-13 09:28:35 +0000

    new task ten

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,37 +1,17 @@
-. Python - Inheritance, task 8 """
-
-
+#!/usr/bin/python3
+'''Defines the class Square.'''
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """Inherits from Rectangle and thus from BaseGeometry, for use with
-    rectangular constructs of equal sides.
+    '''Implements the Square functions.'''
 
-    Args:
-        size (int): length of side of square
-
-    Attributes:
-        __size (int): length of side of square
-
-    """
     def __init__(self, size):
+        '''Attributes of the class Square.
+
+        Args:
+           size (int): The size of Square.
+        '''
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
-
-    def area(self):
-        """Returns square area as size * size.
-
-        * Required by task instructions, but overloading Rectangle.area() is
-        redundant here, as it has the same result.
-
-        Attributes:
-            __size (int): length of side of square
-
-        Returns:
-            __size ** 2
-
-        """
-        return self.__size ** 2
-'''
